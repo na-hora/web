26c42f84b4b389d6561cd1156a45bf628afc2b67
Alexandre Hartmann <alexandre.hartmann@o2obots.com>
2024-06-17 17:06:26 -0300

    feat: react query

---
+++ src/providers.tsx
@@ -0,0 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+export const ApplicationProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
