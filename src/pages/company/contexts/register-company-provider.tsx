5f42c3a3a1b0f93ef5e3d9ef309e79b2d6b42553
Alexandre Hartmann <alexandrehrtm@protonmail.com>
2024-06-20 21:27:17 -0300

    feat: search zip code

--- src/pages/company/contexts/register-company-provider.tsx
+++ src/pages/company/contexts/register-company-provider.tsx
@@ -1,47 +1,60 @@
-import { createContext, useContext, useState } from "react";
+import { Form, FormInstance } from "antd"
+import { createContext, useContext, useState } from "react"
 
 type RegisterCompanyFormParams = {
-  name: string;
-  fantasyName: string;
-  cnpj: string;
-  email: string;
-  phone: string;
-  password: string;
+  name: string
+  fantasyName: string
+  cnpj: string
+  email: string
+  phone: string
+  password: string
   address?: {
-    zipCode: string;
-    cityId: number;
-    neighborhood: string;
-    street: string;
-    number: number;
-    complement: string;
-  };
-  validator: string;
-};
+    zipCode: string
+    cityId: number
+    neighborhood: string
+    street: string
+    number: number
+    complement: string
+  }
+  validator: string
+}
 
 export type Params = {
-  registerCompanyFormData: RegisterCompanyFormParams;
+  registerCompanyFormData: RegisterCompanyFormParams
   setRegisterCompanyFormData: React.Dispatch<
     React.SetStateAction<RegisterCompanyFormParams>
-  >;
-};
+  >
+  form: FormInstance
+  zipCodeToSearch: string
+  setZipCodeToSearch: React.Dispatch<React.SetStateAction<string>>
+}
 
-export const RegisterCompanyContext = createContext<Params>({} as Params);
+export const RegisterCompanyContext = createContext<Params>({} as Params)
 
 export const RegisterCompanyProvider: React.FC<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }> = ({ children }) => {
   const [registerCompanyFormData, setRegisterCompanyFormData] =
-    useState<RegisterCompanyFormParams>({} as RegisterCompanyFormParams);
+    useState<RegisterCompanyFormParams>({} as RegisterCompanyFormParams)
+  const [zipCodeToSearch, setZipCodeToSearch] = useState("")
+
+  const [form] = Form.useForm()
 
   return (
     <RegisterCompanyContext.Provider
-      value={{ registerCompanyFormData, setRegisterCompanyFormData }}
+      value={{
+        registerCompanyFormData,
+        setRegisterCompanyFormData,
+        form,
+        zipCodeToSearch,
+        setZipCodeToSearch,
+      }}
     >
       {children}
     </RegisterCompanyContext.Provider>
-  );
-};
+  )
+}
 
 export const useRegisterCompanyContext = (): Params => {
-  return useContext(RegisterCompanyContext);
-};
+  return useContext(RegisterCompanyContext)
+}
