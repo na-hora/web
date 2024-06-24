3ddee2e834013270acb3ebe7a1f4c83c5d5a6cd9
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-06-24 18:32:08 -0300

    fix: teste not found routes

---
+++ src/components/errors/not-found/index.tsx
@@ -0,0 +1,41 @@
+import { Button, Result } from 'antd'
+
+export const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#f5f5f5',
+      }}
+    >
+      {/* .main {
+  height: 100%;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background-color: #f5f5f5;
+} */}
+      <Result
+        icon={<img src='/logo.svg' alt='' style={{ width: '150px' }} />}
+        title={<h3>Opss... Esta página não foi encontrada</h3>}
+        style={{
+          maxWidth: '500px',
+        }}
+        extra={
+          <div>
+            <p>
+              Você pode retornar para a tela inicial ou entrar em contato com o
+              nosso suporte.
+            </p>
+            <Button type='primary' key='console' href='/'>
+              Início
+            </Button>
+          </div>
+        }
+      />
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { AdminLoginPage } from '@/pages/admin/login'
 import { RegisterCompany } from '@/pages/company/register'
 import { SuccessCompanyPage } from '@/pages/company/register-success'
 import { Dashboard } from '@/pages/dashboard'
+import { NotFoundPage } from '@/pages/errors/not-found'
 import { Home } from '@/pages/home'
 import { createBrowserRouter } from 'react-router-dom'
 
@@ -9,51 +10,38 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+    ErrorBoundary: () => <NotFoundPage />,
   },
   {
     path: '/appointment',
     element: <div>Appointment</div>,
   },
   {
-    path: '/company',
-    children: [
-      {
-        path: 'register',
-        element: <RegisterCompany />,
-        children: [
-          {
-            path: 'success',
-            element: <SuccessCompanyPage />,
-          },
-        ],
-      },
-    ],
+    path: '/company/register',
+    element: <RegisterCompany />,
   },
   {
-    path: '/admin',
-    children: [
-      {
-        path: 'login',
-        element: <AdminLoginPage />,
-      },
-      {
-        path: 'dashboard',
-        element: <Dashboard />,
-        children: [
-          {
-            path: 'home',
-            element: <div>Home</div>,
-          },
-          {
-            path: 'company',
-            element: <div>Company</div>,
-          },
-          {
-            path: 'profile',
-            element: <div>Profile</div>,
-          },
-        ],
-      },
-    ],
+    path: '/company/register/success',
+    element: <SuccessCompanyPage />,
+  },
+  {
+    path: '/admin/login',
+    element: <AdminLoginPage />,
+  },
+  {
+    path: '/admin/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/admin/dashboard/home',
+    element: <div>Home</div>,
+  },
+  {
+    path: '/admin/dashboard/company',
+    element: <div>Company</div>,
+  },
+  {
+    path: '/admin/dashboard/profile',
+    element: <div>Profile</div>,
   },
 ])
