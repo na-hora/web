89b1239bb5ac742caf888b4f9061ae18d26ec59c
Alexandre Hartmann <alexandrehrtm@protonmail.com>
2024-07-02 20:58:22 -0300

    refactor: auth guard

--- src/routes/guards/auth-guard.tsx
+++ src/routes/guards/auth-guard.tsx
@@ -1,16 +1,10 @@
+import { parseCookies } from 'nookies'
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
-const RouteGuard = ({ children }: { children: React.ReactNode }) => {
-  // Function to get the cookie by name
-  const getCookie = (name: string) => {
-    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))
-    if (match) return match[2]
-    return null
-  }
-
+export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   // Check if the auth_token cookie is present
-  const authToken = getCookie('access-token@na-hora')
+  const authToken = parseCookies()['access-token@na-hora']
 
   // If not present, redirect to the login page
   if (!authToken) {
@@ -20,5 +14,3 @@ const RouteGuard = ({ children }: { children: React.ReactNode }) => {
   // Otherwise, render the children components
   return children
 }
-
-export default RouteGuard
