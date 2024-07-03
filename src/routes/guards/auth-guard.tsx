3f597626b548de260a491023b1c630d1140ce9c2
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-07-03 08:22:26 -0300

    chore: remove unused comments

--- src/routes/guards/auth-guard.tsx
+++ src/routes/guards/auth-guard.tsx
@@ -3,14 +3,11 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
-  // Check if the auth_token cookie is present
   const authToken = parseCookies()['access-token@na-hora']
 
-  // If not present, redirect to the login page
   if (!authToken) {
     return <Navigate to='/admin/login' replace />
   }
 
-  // Otherwise, render the children components
   return children
 }
