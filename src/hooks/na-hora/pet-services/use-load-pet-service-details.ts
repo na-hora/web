706b8bb2da4bc68ab3cc5470e248965d3f009f7b
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-08-27 21:52:30 -0300

    fix: const name

--- src/hooks/na-hora/pet-services/use-load-pet-service-details.ts
+++ src/hooks/na-hora/pet-services/use-load-pet-service-details.ts
@@ -1,10 +1,18 @@
 import { useHooks } from '@/hooks'
 import { UseQueryResult } from '@tanstack/react-query'
 
-type UseLoadPetServicesResponse = Array<{
+type UseLoadPetServicesResponse = {
   id: number
   name: string
-}>
+  paralellism: number
+  configurations: {
+    id: number
+    companyPetHairId: number
+    companyPetSizeId: number
+    price: number
+    executionTime: number
+  }[]
+}
 
 export const useLoadPetServiceDetails = (
   serviceId: number,
