10d91de3805a7fe11ed2ef00132f7f7a55565747
Alexandre Hartmann <alexandrehrtm@gmail.com>
2024-10-26 11:33:28 -0300

    fix: calendar header date

--- src/components/dashboard/appointments/create-appointment-modal/index.tsx
+++ src/components/dashboard/appointments/create-appointment-modal/index.tsx
@@ -12,7 +12,7 @@ export const CreateAppointmentModal = () => {
     const data = new Date(date)
 
     const dia = String(data.getUTCDate()).padStart(2, '0')
-    const mes = String(data.getUTCMonth() + 1).padStart(2, '0') // Os meses são indexados a partir de 0
+    const mes = String(data.getUTCMonth() + 1).padStart(2, '0')
     const ano = data.getUTCFullYear()
     const horas = String(data.getUTCHours()).padStart(2, '0')
     const minutos = String(data.getUTCMinutes()).padStart(2, '0')
