f09f3328221873362778965326f89afd5758a456
Alexandre Hartmann <alexandrehrtm@protonmail.com>
2024-10-26 15:17:29 -0300

    fix: calendar hour format

--- src/hooks/na-hora/appointments/types/load.ts
+++ src/hooks/na-hora/appointments/types/load.ts
@@ -1,13 +1,10 @@
 export type Appointment = {
   id: string
-  petName: string
   serviceName: string
   startTime: string
   totalTime: number
   totalPrice: number
-  paymentMode: string
   canceled: boolean
-  cancelationReason: string
 }
 
 export type LoadAppointmentsResponse = {
