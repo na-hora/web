23f4016da2cee0e5ccc16b85353d9481e16de7ea
Alexandre Hartmann <alexandrehrtm@gmail.com>
2024-10-26 10:48:50 -0300

    feat: fetch appointments by date

--- src/hooks/na-hora/appointments/use-load-appointments.ts
+++ src/hooks/na-hora/appointments/use-load-appointments.ts
@@ -1,15 +1,37 @@
 import { useHooks } from '@/hooks'
+import { useAppointmentsContext } from '@/pages/dashboard/appointments/contexts/appointments-provider'
+import { convertDateToISOString } from '@/utils/time'
 import { UseQueryResult } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { LoadAppointmentsResponse } from './types/load'
 
 export const useLoadAppointments =
   (): UseQueryResult<LoadAppointmentsResponse> => {
     const { useGetData } = useHooks()
-    // const { selectedDateRangeText } = useAppointmentsContext()
+    const [dates, setDates] = useState<{
+      start: string | null
+      end: string | null
+    }>({
+      start: null,
+      end: null,
+    })
+    const { selectedDateRangeText } = useAppointmentsContext()
+
+    useEffect(() => {
+      if (!selectedDateRangeText) return
+
+      const [startDate, endDate] = selectedDateRangeText.split(' - ')
+      if (!startDate || !endDate) return
+
+      const start = convertDateToISOString(startDate)
+      const end = convertDateToISOString(endDate)
+      setDates({ start, end })
+    }, [selectedDateRangeText])
 
     return useGetData({
-      url: `${
-        import.meta.env.VITE_API_URL
-      }/appointments?startDate=2024-10-19T11:00:00-03:00&endDate=2024-10-30T12:00:00-03:00`,
+      url: `${import.meta.env.VITE_API_URL}/appointments?startDate=${
+        dates.start
+      }&endDate=${dates.end}`,
+      enabled: !!selectedDateRangeText && !!dates.start && !!dates.end,
     })
   }
