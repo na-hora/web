b57134884538509277988b0a7c272dc6751b5587
Alexandre Hartmann <alexandrehrtm@protonmail.com>
2024-11-17 10:54:55 -0300

    feat: delete pet type

--- src/hooks/na-hora/pet-type/use-load-pet-types.ts
+++ src/hooks/na-hora/pet-type/use-load-pet-types.ts
@@ -1,12 +1,10 @@
 import { useHooks } from '@/hooks'
 import { UseQueryResult } from '@tanstack/react-query'
+import { LoadPetTypesResponse } from './types/load.type'
 
 export const useLoadPetTypes = (
   companyId: string,
-): UseQueryResult<{
-  id: number
-  name: string
-}> => {
+): UseQueryResult<LoadPetTypesResponse> => {
   const { useGetData } = useHooks()
   return useGetData({
     url: `${import.meta.env.VITE_API_URL}/pet-type?companyId=${companyId}`,
