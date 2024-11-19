7771c04d799392ddcc188aef63236e0a9ff3920d
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-11-19 15:14:58 -0300

    feat: add basic services page

--- src/hooks/na-hora/pet-services/use-load-pet-services.ts
+++ src/hooks/na-hora/pet-services/use-load-pet-services.ts
@@ -2,10 +2,11 @@ import { useHooks } from '@/hooks'
 import { UseQueryResult } from '@tanstack/react-query'
 import { LoadPetServicesResponse } from './types/list.type'
 
-export const useLoadPetServices =
-  (): UseQueryResult<LoadPetServicesResponse> => {
-    const { useGetData } = useHooks()
-    return useGetData({
-      url: `${import.meta.env.VITE_API_URL}/services/pet`,
-    })
-  }
+export const useLoadPetServices = (
+  companyId: string,
+): UseQueryResult<LoadPetServicesResponse> => {
+  const { useGetData } = useHooks()
+  return useGetData({
+    url: `${import.meta.env.VITE_API_URL}/services/pet?companyId=${companyId}`,
+  })
+}
