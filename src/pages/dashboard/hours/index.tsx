33a068beb69574f5a84b2aaa955ae789aed0467b
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-11-28 17:28:49 -0300

    feat: add styles to schedule hours screen

--- src/pages/dashboard/hours/index.tsx
+++ src/pages/dashboard/hours/index.tsx
@@ -3,12 +3,6 @@ import { Button, Col, Form, Row, TimePicker, Typography } from 'antd'
 import dayjs from 'dayjs'
 import { useState } from 'react'
 
-type Schedule = {
-  weekDay: number
-  startHour: number
-  endHour: number
-}
-
 const daysOfWeek = [
   { label: 'Domingo', value: 0 },
   { label: 'Segunda-feira', value: 1 },
@@ -19,13 +13,19 @@ const daysOfWeek = [
   { label: 'Sábado', value: 6 },
 ]
 
+interface Schedule {
+  weekDay: number
+  startMinute: number
+  endMinute: number
+}
+
 export const DashboardHours = () => {
   const [schedules, setSchedules] = useState<{ [key: number]: Schedule[] }>(
     () =>
       daysOfWeek.reduce(
         (acc, day) => ({
           ...acc,
-          [day.value]: [{ weekDay: day.value, startHour: 6, endHour: 12 }],
+          [day.value]: [],
         }),
         {},
       ),
@@ -36,7 +36,7 @@ export const DashboardHours = () => {
       ...prev,
       [weekDay]: [
         ...prev[weekDay],
-        { weekDay, startHour: 6, endHour: 12 }, // Default new period
+        { weekDay, startMinute: 480, endMinute: 720 }, // Default 08:00 to 12:00
       ],
     }))
   }
@@ -52,16 +52,13 @@ export const DashboardHours = () => {
     weekDay: number,
     index: number,
     field: keyof Schedule,
-    value: number | dayjs.Dayjs,
+    value: dayjs.Dayjs | null,
   ) => {
+    if (!value) return
+    const totalMinutes = value.hour() * 60 + value.minute()
     setSchedules((prev) => {
       const updatedSchedules = [...prev[weekDay]]
-      if (field === 'startHour' || field === 'endHour') {
-        updatedSchedules[index][field] =
-          dayjs(value).hour() + dayjs(value).minute() / 60
-      } else {
-        updatedSchedules[index][field] = value as number
-      }
+      updatedSchedules[index][field] = totalMinutes
       return { ...prev, [weekDay]: updatedSchedules }
     })
   }
@@ -72,89 +69,119 @@ export const DashboardHours = () => {
   }
 
   return (
-    <Form
-      layout='vertical'
-      // style={{ padding: '24px', background: '#f9f9f9', borderRadius: '8px' }}
-    >
-      {daysOfWeek.map((day) => (
-        <Col
-          key={day.value}
-          style={{
-            marginBottom: 24,
-            background: '#fff',
-            padding: 16,
-            borderRadius: 8,
-            boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
-          }}
-        >
-          <Typography.Title level={5}>{day.label}</Typography.Title>
-          {schedules[day.value].map((schedule, index) => (
-            <Row gutter={[16, 16]} key={index} align='middle'>
-              <Col xs={24} sm={10}>
-                <Form.Item label={index === 0 ? 'Hora inicial' : ''}>
-                  <TimePicker
-                    format='HH:mm'
-                    value={dayjs()
-                      .hour(Math.floor(schedule.startHour))
-                      .minute((schedule.startHour % 1) * 60)}
-                    onChange={(value) =>
-                      handleScheduleChange(
-                        day.value,
-                        index,
-                        'startHour',
-                        value!,
-                      )
-                    }
-                    style={{ width: '20%' }}
-                  />
-                </Form.Item>
-              </Col>
-              <Col xs={24} sm={10}>
-                <Form.Item label={index === 0 ? 'Hora final' : ''}>
-                  <TimePicker
-                    format='HH:mm'
-                    value={dayjs()
-                      .hour(Math.floor(schedule.endHour))
-                      .minute((schedule.endHour % 1) * 60)}
-                    onChange={(value) =>
-                      handleScheduleChange(day.value, index, 'endHour', value!)
-                    }
-                    style={{ width: '20%' }}
-                  />
-                </Form.Item>
-              </Col>
-              <Col xs={24} sm={4}>
-                {schedules[day.value].length > 1 && (
-                  <Button
-                    danger
-                    icon={<DeleteOutlined />}
-                    onClick={() => removeSchedule(day.value, index)}
-                    style={{ width: '100%' }}
+    <>
+      <h1 style={{ marginBottom: '4px' }}>Horários</h1>
+      <span>
+        Defina aqui os horários que ficarão disponíveis para seus clientes
+        agendarem atendimentos
+      </span>
+      <Row justify='center'>
+        <Col span={10}>
+          <Form layout='vertical'>
+            {daysOfWeek.map((day) => (
+              <div
+                key={day.value}
+                style={{
+                  marginBottom: 24,
+                  padding: '4px 16px',
+                  background: '#fff',
+                  borderRadius: 8,
+                  boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
+                  textAlign: 'center',
+                  alignItems: 'center',
+                  width: '500px',
+                }}
+              >
+                <Typography.Title level={5} style={{ marginBottom: 16 }}>
+                  {day.label}
+                </Typography.Title>
+                {schedules[day.value].map((schedule, index) => (
+                  <Col
+                    key={index}
+                    style={{
+                      display: 'flex',
+                      gap: 16,
+                      alignItems: 'center',
+                      marginBottom: 16,
+                      justifyContent: 'center',
+                    }}
                   >
-                    Remover
-                  </Button>
-                )}
-              </Col>
-            </Row>
-          ))}
-          <Button
-            type='dashed'
-            onClick={() => addSubline(day.value)}
-            style={{ marginTop: 16, width: '20%' }}
-            icon={<PlusOutlined />}
-          >
-            Adicionar horário
-          </Button>
+                    <Form.Item
+                      label={index === 0 ? 'Hora inicial' : ''}
+                      style={{ marginBottom: 0 }}
+                    >
+                      <TimePicker
+                        format='HH:mm'
+                        value={dayjs()
+                          .hour(Math.floor(schedule.startMinute / 60))
+                          .minute(schedule.startMinute % 60)}
+                        onChange={(value) =>
+                          handleScheduleChange(
+                            day.value,
+                            index,
+                            'startMinute',
+                            value,
+                          )
+                        }
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      label={index === 0 ? 'Hora final' : ''}
+                      style={{ marginBottom: 0 }}
+                    >
+                      <TimePicker
+                        format='HH:mm'
+                        value={dayjs()
+                          .hour(Math.floor(schedule.endMinute / 60))
+                          .minute(schedule.endMinute % 60)}
+                        onChange={(value) =>
+                          handleScheduleChange(
+                            day.value,
+                            index,
+                            'endMinute',
+                            value,
+                          )
+                        }
+                      />
+                    </Form.Item>
+                    <Button
+                      danger
+                      icon={<DeleteOutlined />}
+                      onClick={() => removeSchedule(day.value, index)}
+                      style={{
+                        height: 'fit-content',
+                        alignSelf: 'flex-end',
+                      }}
+                    >
+                      Remover
+                    </Button>
+                  </Col>
+                ))}
+                <Button
+                  type='dashed'
+                  onClick={() => addSubline(day.value)}
+                  icon={<PlusOutlined />}
+                  style={{
+                    marginTop: 8,
+                    textAlign: 'center',
+                    width: '100%',
+                  }}
+                >
+                  Adicionar horário
+                </Button>
+              </div>
+            ))}
+            <Button
+              type='primary'
+              onClick={handleSave}
+              block
+              style={{ marginTop: 24 }}
+            >
+              Salvar Horários
+            </Button>
+          </Form>
         </Col>
-      ))}
-      <Button
-        type='primary'
-        onClick={handleSave}
-        block
-        style={{ marginTop: 24 }}
-      >
-        Salvar Horários
-      </Button>
-    </Form>
+      </Row>
+    </>
   )
 }
