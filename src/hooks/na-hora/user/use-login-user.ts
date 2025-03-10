48a02bdc277d0012b9a2635188849d0eaf3964f9
ArthurRFr <arthur_fronza2011@hotmail.com>
2025-03-10 17:36:31 -0300

    test: ci

--- src/hooks/na-hora/user/use-login-user.ts
+++ src/hooks/na-hora/user/use-login-user.ts
@@ -5,6 +5,8 @@ import { LoginUserRequestBody, LoginUserResponse } from './types/login.type'
 export const useLoginUser = () => {
   const { usePostData } = useHooks()
 
+  console.log({ host: import.meta.env.VITE_API_URL })
+
   return usePostData<TPostDataParams<LoginUserRequestBody>, LoginUserResponse>({
     url: `${import.meta.env.VITE_API_URL}/users/login`,
     mutationKey: 'na-hora:login-user',
